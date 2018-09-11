a91d106524b0ae82fc953a798f1e05b9f1e792cf
Ethan <kkojaeh@gmail.com>
2018-09-11 11:31:35 +0900

    메뉴 삭제

--- bom-api/src/main/java/pico/erp/bom/ROLE.java
+++ bom-api/src/main/java/pico/erp/bom/ROLE.java
@@ -1,28 +1,18 @@
 package pico.erp.bom;
 
-import java.util.Collections;
-import java.util.Set;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import pico.erp.shared.data.Menu;
 import pico.erp.shared.data.Role;
 
 @RequiredArgsConstructor
 public enum ROLE implements Role {
 
-  // BOM_MANAGER(Stream.of(MENU.BOM_MANAGEMENT).collect(Collectors.toSet())),
-  BOM_MANAGER(Collections.emptySet()),
-  BOM_ACCESSOR(Collections.emptySet());
+  BOM_MANAGER,
+  BOM_ACCESSOR;
 
   @Id
   @Getter
   private final String id = name();
 
-  @Transient
-  @Getter
-  @NonNull
-  private Set<Menu> menus;
 }
